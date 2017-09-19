cc3c46e424cb0fa6976be37c1e77eae6daf5f47a
Mark Helt and Riki Montgomery <mark+riki@>
2017-09-19 11:58:34 -0700

    added front-end logic for admin form and added a method to link the doctor's id to the patient's

--- app.rb
+++ app.rb
@@ -1,15 +1,34 @@
-require ('sinatra')
-require ('pry')
-require ('sinatra/reloader')
-also_reload('.lib/**/*.rb')
-require ('./lib/definer')
+require 'sinatra'
+require 'pry'
+require 'sinatra/reloader'
+also_reload '.lib/**/*.rb'
+require './lib/patient'
+require './lib/doctor'
+require 'pg'
+
+DB = PG.connect({dbname: 'doctors_office'})
 
 get('/') do
 
-  erb(:input)
+  erb(:home)
 end
 
-post('/') do
+post('/patient') do
+  name = params['name']
+  birthday = params['birthday']
+  doctor_id = params['doctor_id']
 
-  erb(:input)
+  patient = Patient.new({name: name, birthday: birthday, doctor_id: doctor_id, id: nil})
+  patient.save
+  erb(:home)
+end
+
+post('/doctor') do
+  name = params['name']
+  specialty = params['specialty']
+
+
+  doctor = Doctor.new({name: name, specialty: specialty, id: nil})
+  doctor.save
+  erb(:home)
 end
--- lib/doctor.rb
+++ lib/doctor.rb
@@ -27,4 +27,17 @@ class Doctor
   def ==(another_doctor)
     self.name().==(another_doctor.name).&(self.specialty().==(another_doctor.specialty())).&(self.id().==(another_doctor.id()))
   end
+
+  def patients
+    doctor_patients = []
+    patients = DB.exec("SELECT * FROM patients WHERE doctor_id = #{self.id()};")
+    patients.each() do |patient|
+      name = patient.fetch("name")
+      birthday = patient.fetch("birthday")
+      doctor_id = patient.fetch("doctor_id").to_i
+      id = patient.fetch("id").to_i
+      doctor_patients.push(Patient.new({name: name, birthday: birthday, doctor_id: doctor_id, id: id}))
+    end
+    doctor_patients
+  end
 end
